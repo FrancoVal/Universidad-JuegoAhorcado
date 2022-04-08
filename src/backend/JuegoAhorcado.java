5535c0b77ce49ce701c418ef59a6d0ed24d013ad
Ignacio Tula <itula@logos.net.ar>
2022-04-08 12:35:23 -0300

    Nacen las props y el constructor de JuegoAhorcado

--- src/backend/JuegoAhorcado.java
+++ src/backend/JuegoAhorcado.java
@@ -2,10 +2,39 @@ package backend;
 
 public class JuegoAhorcado {
 
-	private Adivinanza desafio;
+	private Idioma idioma;
+	private Dificultad dificultad;
+	private Adivinanza adivinanza;
+	
+	private int intentosFallidosRestantes;
+	private char[] listadoLetrasFallidas;
+	
+	private String palabra;
 	
 	
+	public JuegoAhorcado(Idioma idioma, Dificultad dificultad) {
+		
+		this.crearCaracBasicas(idioma, dificultad);
+		this.crearAdivinanza();
+
+	}
 	
+	private void crearCaracBasicas(Idioma idioma, Dificultad dificultad) {
+		
+		this.idioma = idioma;
+		this.dificultad = dificultad;
+		this.intentosFallidosRestantes = dificultad.intentos();
+		this.listadoLetrasFallidas = new char[this.intentosFallidosRestantes];
+		
+	}
+	
+	private void crearAdivinanza() {
+		
+		Diccionario diccionario = new Diccionario(this.idioma, this.dificultad);
+		this.palabra = diccionario.elegirPalabraAlAzar();
+		adivinanza = new Adivinanza(this.palabra);
+		
+	}
 	
 	
 }
