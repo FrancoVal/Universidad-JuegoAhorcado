ed77120c9f1c5a88d0fd10a29fd133b829220147
Ignacio Tula <itula@logos.net.ar>
2022-04-08 12:35:01 -0300

    Dificultad con intentos
    La definición de dificultad ahora incluye de manera innata la cantidad de intentos posibles que se pueden errar

--- src/backend/Dificultad.java
+++ src/backend/Dificultad.java
@@ -1,20 +1,26 @@
 package backend;
 
 public enum Dificultad {
-	FACIL("Facil"),
-	MEDIO("Medio"),
-	DIFICIL("Dificil");
+	FACIL("Facil", 8),
+	MEDIO("Medio", 7),
+	DIFICIL("Dificil", 6);
 	
 	
 private String nombre;
+private int intentosFallidosPosibles;
 	
 	
-	private Dificultad(String nombre) {
+	private Dificultad(String nombre, int intentos) {
 		this.nombre = nombre;
+		this.intentosFallidosPosibles = intentos;
 	}
 	
 	
 	public String nombre() {
 		return this.nombre;
+	}
+	
+	public int intentos() {
+		return this.intentosFallidosPosibles;
 	}
 }
