208c52578da6315e5a5c066e8531ec85031a75e4
Ignacio Tula <itula@logos.net.ar>
2022-04-10 04:33:08 -0300

    Principio de diseño para recibir una palabra que desafie a la IA

---
+++ src/backend/NoPuedeJugarAISinoEsDesafiadaException.java
@@ -0,0 +1,7 @@
+package backend;
+
+public class NoPuedeJugarAISinoEsDesafiadaException extends Exception {
+    public NoPuedeJugarAISinoEsDesafiadaException(String mensaje) {
+        super(mensaje);
+    }
+}
--- src/backend/Torneo.java
+++ src/backend/Torneo.java
@@ -13,6 +13,7 @@ public class Torneo {
   final int maximoDeSets = 3;
 
   private JuegoAhorcado ahorcado;
+  private String desafioPropuestoPorJugador;
 
   public enum Jugador {
     JUGADOR, AI;
@@ -31,10 +32,21 @@ public class Torneo {
   }
 
   private void nuevoAhorcado() {
-	  if (juegaElHumano())
+	  if (juegaElHumano()) {
+		  this.ahorcado = new JuegoAhorcado(this.idiomaActual, this.dificultadActual);
+	  } 
+	  else if (this.haSidoDesafiadaLaAI()) {
+			  this.ahorcado = new JuegoAhorcado(this.idiomaActual, this.dificultadActual, this.desafioPropuestoPorJugador);
+			  this.desafioPropuestoPorJugador = null;
+		  } else {
+			  this.ahorcado = null;
+		  }
+		 
+	  
   }
   public void probarLetra(char letra)
-      throws NoHayMasIntentosException, NoTieneChancesException, NoPuedeRepetirLetraException {
+      throws NoHayMasIntentosException, NoTieneChancesException, 
+      NoPuedeRepetirLetraException, NoPuedeJugarAISinoEsDesafiadaException {
 
     /*
      * Esto se está refactoreando más abajo, de recibir una exception similar
@@ -55,7 +67,15 @@ public class Torneo {
           "Según la dificultad, este set ayuda al jugador a no cometer el error de repetir letras usadas \n" +
           "Y la letra indicada está repetida");
     }
-
+    
+    if (this.ahorcado == null && !this.juegaElHumano()) {
+    	throw new NoPuedeJugarAISinoEsDesafiadaException("No se puede probar letra \n" +
+    	          "Es turno de la AI pero no se pudo crear un ahorcado para que juegue \n" +
+    	          "porque el usuario no creó ningún desafio");
+    }
+    
+    
+    
     try {
       this.ahorcado.turno(letra);
 
@@ -174,6 +194,20 @@ public class Torneo {
   public boolean juegaElHumano() {
 	  return this.turnoActual.equals(Jugador.JUGADOR) ? true : false;
   }
+  
+  public void desafiarAI(String palabra) {
+	  
+	  this.desafioPropuestoPorJugador = palabra;
+	  
+	  if (this.ahorcado == null && !this.juegaElHumano()) {
+		  this.nuevoAhorcado();
+	  }
+  }
+  
+  public boolean haSidoDesafiadaLaAI() {
+	  return this.desafioPropuestoPorJugador == null ? false : true;
+  }
+  
   
 
 }
